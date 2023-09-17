0dffcfd22c2eaf4c8b80d01f338ad09e5b503f5f
Rajko Culibrk <r.culibrk@ponthus.nl>
2023-09-17 17:47:44 +0200

    End of section 6

--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -6,7 +6,7 @@ namespace SearchService.Models
     {
         public int ReservePrice { get; set; }
         public string Seller { get; set; }
-        public string Winner { get; }
+        public string Winner { get; set; }
         public int SoldAmount { get; set; }
         public int CurrentHighBid { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
