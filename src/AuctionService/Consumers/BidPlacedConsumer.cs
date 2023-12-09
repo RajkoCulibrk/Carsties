1352340f1ed73ce1a320dc97bcd6cad6ea30ca49
Rajko Culibrk <r.culibrk@ponthus.nl>
2023-12-09 23:15:40 +0100

    Added Grpc

--- src/AuctionService/Consumers/BidPlacedConsumer.cs
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -15,15 +15,21 @@ namespace AuctionService.Consumers
 
         public async Task Consume(ConsumeContext<BidPlaced> context)
         {
-            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+            try {
+                var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
-            if(auction.CurrentHighBid == null 
-                || context.Message.BidStatus.Contains("Accepted")
-                && context.Message.Amount > auction.CurrentHighBid
-                )
+                if(auction.CurrentHighBid == null 
+                   || context.Message.BidStatus.Contains("Accepted")
+                   && context.Message.Amount > auction.CurrentHighBid
+                  )
+                {
+                    auction.CurrentHighBid = context.Message.Amount;
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
             {
-                auction.CurrentHighBid = context.Message.Amount;
-                await _dbContext.SaveChangesAsync();
+                
             }
         }
     }
